858a173f7edf7e68df5a076c940032e780992a3d
epnev <eap2111@columbia.edu>
2015-10-05 14:38:08 -0400

    included rearranging of struct P

--- utilities/order_ROIs.m
+++ utilities/order_ROIs.m
@@ -1,4 +1,4 @@
-function [A_or,C_or,srt] = order_ROIs(A,C)
+function [A_or,C_or,P_or,srt] = order_ROIs(A,C,P)
 
 % ordering of the found components based on their maximum temporal
 % activation and their size (through their l4 norm)
@@ -12,3 +12,9 @@ mC = max(C,[],2);
 [~,srt] = sort(mC.*nA4','descend');
 A_or = A(:,srt);
 C_or = C(srt,:);
+P_or = P;
+
+if isfield(P,'gn'); P_or.gn=P.gn(srt); end
+if isfield(P,'b'); P_or.b=P.b(srt); end
+if isfield(P,'c1'); P_or.c1=P.c1(srt); end
+if isfield(P,'neuron_sn'); P_or.neuron_sn=P.neuron_sn(srt); end
