d523cf32624684bdf56e0ec3698a800c09006096
eftychios <epnevmatikakis@gmail.com>
2015-11-10 16:55:20 -0500

    Update com.m
    just commenting

--- utilities/com.m
+++ utilities/com.m
@@ -1,6 +1,12 @@
 function cm = com(A,d1,d2)
 
 % center of mass calculation
+% inputs:
+% A: d X nr matrix, each column in the spatial footprint of a neuron
+% d1, d2: the dimensions of the 2-d field of view
+
+% output:
+% cm: nr x 2 matrix, with the center of mass coordinates
 
 nr = size(A,2);
 Coor.x = kron(ones(d2,1),(1:d1)');
