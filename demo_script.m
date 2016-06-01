1f0944d02e82139ce4519839908b4b201a268d21
epnev <eap2111@columbia.edu>
2016-06-01 18:34:44 -0400

    remove third output from extract_DF_F

--- demo_script.m
+++ demo_script.m
@@ -93,7 +93,7 @@ P.p = p;    % restore AR value
 
 [A_or,C_or,S_or,P] = order_ROIs(A2,C2,S2,P); % order components
 K_m = size(C_or,1);
-[C_df,~,S_df] = extract_DF_F(Yr,[A_or,b2],[C_or;f2],S_or,K_m+1); % extract DF/F values (optional)
+[C_df,~] = extract_DF_F(Yr,[A_or,b2],[C_or;f2],S_or,K_m+1); % extract DF/F values (optional)
 
 contour_threshold = 0.95;                       % amount of energy used for each component to construct contour plot
 figure;
