21f73e3f995b161881287301431107a585048ad0
epnev <eap2111@columbia.edu>
2016-09-28 12:05:31 -0400

    new run_CNMF_patches
    1. memory mapped is not necessary (and by default not performed)
    2. rank > 1 background is allowed through options.gnb

--- run_CNMF_patches.m
+++ run_CNMF_patches.m
@@ -6,8 +6,7 @@ function [A,b,C,f,S,P,RESULTS,YrA] = run_CNMF_patches(data,K,patches,tau,p,optio
 % with reduced memory requirements. Processing in patches also allows the
 % identification of weaker neurons without the need of normalization.
 % The components are also classified by retaining only the components that
-% significantly overlap with the active pixels. The active pixels are
-% determined by classifying the PSD of the data.
+% correlate well with the raw data through classify_comp_corr.m
 
 % INPUTS:
 
@@ -16,8 +15,8 @@ function [A,b,C,f,S,P,RESULTS,YrA] = run_CNMF_patches(data,K,patches,tau,p,optio
 %   data.Yr     (the data matrix reshaped in 2d format)
 %   data.sizY   (dimensions of the original dataset)
 %   data.nY     (minimum value of dataset)
-% OR the original dataset in 3d/4d format in which case a memory mapped
-%   file is created
+% OR the original dataset in 3d/4d format in which case the user chooses 
+% whether to create a memory mapped file 
 
 % K      :   number of components to be found in each patch
 % patches:   cell array containing the start and end points of each patch   
@@ -35,26 +34,41 @@ function [A,b,C,f,S,P,RESULTS,YrA] = run_CNMF_patches(data,K,patches,tau,p,optio
 % RESULTS:   Results of the CNMF algorithm on individual patches
 % YrA    :   Residual signal at the level of each component
 
-% Author: Eftychios A. Pnevmatikakis, Simons Foundation, 2016
+% Author: Eftychios A. Pnevmatikakis, Simons Foundation, 2015, 2016
+
+defoptions = CNMFSetParms;
+
+if nargin < 6 || isempty(options)
+    options = defoptions;
+end
+
+if ~isfield(options,'cluster_pixels') || isempty(options.cluster_pixels); options.cluster_pixels = defoptions.cluster_pixels; end
+if ~isfield(options,'create_memmap') || isempty(options.create_memmap); options.create_memmap = defoptions.create_memmap; end
+if ~isfield(options,'gnb') || isempty(options.gnb); options.gnb = defoptions.gnb; end
+if ~isfield(options,'classify_comp') || isempty(options.classify_comp); options.classify_comp = defoptions.classify_comp; end
 
 memmaped = isobject(data);
 if memmaped
     sizY = data.sizY;
-else    % create a memory mapped object named data_file.mat
+else    % create a memory mapped object named data_file.mat    
     Y = data;
     clear data;
     sizY = size(Y);
     Yr = reshape(Y,prod(sizY(1:end-1)),[]);
     nY = min(Yr(:));
-    %Yr = Yr - nY;    
-    save('data_file.mat','Yr','Y','nY','sizY','-v7.3');
-    data = matfile('data_file.mat','Writable',true);
+    %Yr = Yr - nY;
+    if options.create_memmap
+        save('data_file.mat','Yr','Y','nY','sizY','-v7.3');
+        data = matfile('data_file.mat','Writable',true);
+        memmaped = true;
+    else
+        data = Yr;
+    end
 end
 
-defoptions = CNMFSetParms;
-if nargin < 6 || isempty(options)
-    options = defoptions;
-end
+if ~isfield(options,'d1') || isempty(options.d1); options.d1 = sizY(1); end
+if ~isfield(options,'d2') || isempty(options.d2); options.d2 = sizY(2); end
+if ~isfield(options,'d3') || isempty(options.d3); if length(sizY) == 3; options.d3 = 1; else options.d3 = sizY(3); end; end
 
 if nargin < 5 || isempty(p)
     p = 0;
@@ -68,32 +82,39 @@ if nargin < 3 || isempty(patches)
     patches = construct_patches(sizY(1:end-1),[50,50]);
 end
 
+Yc = cell(length(patches),1);
+if ~memmaped
+    for i = 1:length(patches)
+        if length(sizY) == 3
+            Yc{i} = Y(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),:);
+        else
+            Yc{i} = Y(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6),:);
+        end
+    end
+end        
+
 if nargin < 2 || isempty(K)
     K = 10;
-end
-
-if ~isfield(options,'gnb') || isempty(options.gnb)
-    gnb = 1;
-else
-    gnb = options.gnb;
-end
-    
-if ~isfield(options,'cl_thr') || isempty(options.cl_thr)
-    cl_thr = 0.8;
-else
-    cl_thr = options.cl_thr;
 end
 
 RESULTS(length(patches)) = struct();
 %%
 %parfor_progress(length(patches)); %monitor parfor progress (requires parfor_progress from mathworks file exchange)
-parfor i = 1:length(patches)
+parfor i = 1:length(patches)    
     if length(sizY) == 3
-        Y = data.Y(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),:);
+        if memmaped
+            Y = data.Y(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),:);
+        else
+            Y = Yc{i};
+        end
         [d1,d2,T] = size(Y);
         d3 = 1;
     else
-        Y = data.Y(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6),:);
+        if memmaped
+            Y = data.Y(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6),:);
+        else
+            Y = Yc{i};
+        end
         [d1,d2,d3,T] = size(Y);
     end
     if ~(isa(Y,'single') || isa(Y,'double'));    Y = single(Y);  end
@@ -126,24 +147,26 @@ end
 %parfor_progress(0);
 
 %% combine results into one structure
-fprintf('Combining results from different patches...');
+fprintf('Combining results from different patches... \n');
 d = prod(sizY(1:end-1));
 A = sparse(d,length(patches)*K);
 P.sn = zeros(sizY(1:end-1));
 if isfield(RESULTS(1).P,'sn_ds'); P.sn_ds = zeros(sizY(1:end-1)); end
-P.active_pixels = zeros(sizY(1:end-1));
 IND = zeros(sizY(1:end-1));
 P.b = {};
 P.c1 = {};
 P.gn = {};
 P.neuron_sn = {};
 
-if length(sizY) == 3
-    P.psdx = zeros(patches{end}(2),patches{end}(4),size(RESULTS(1).P.psdx,2));
-else
-    P.psdx = zeros(patches{end}(2),patches{end}(4),patches{end}(6),size(RESULTS(1).P.psdx,2));
+if options.cluster_pixels
+    P.active_pixels = zeros(sizY(1:end-1));
+    if length(sizY) == 3
+        P.psdx = zeros(patches{end}(2),patches{end}(4),size(RESULTS(1).P.psdx,2));
+    else
+        P.psdx = zeros(patches{end}(2),patches{end}(4),patches{end}(6),size(RESULTS(1).P.psdx,2));
+    end
 end
-
+    
 cnt = 0;
 B = sparse(prod(sizY(1:end-1)),length(patches));
 MASK = zeros(sizY(1:end-1));
@@ -166,21 +189,25 @@ for i = 1:length(patches)
         b_temp(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) = reshape(RESULTS(i).b,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1);
         MASK(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) = MASK(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) + 1;
         P.sn(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) = reshape(RESULTS(i).P.sn,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1);
-        if isfield(RESULTS(i).P,'sn_ds'); P.sn_ds(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) = reshape(RESULTS(i).P.sn_ds,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1); end
-        P.active_pixels(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) = P.active_pixels(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) + ...
-            reshape(RESULTS(i).P.active_pixels,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1);
+        if isfield(RESULTS(i).P,'sn_ds'); P.sn_ds(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) = reshape(RESULTS(i).P.sn_ds,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1); end        
         IND(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) = IND(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) + 1;
-        P.psdx(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),:) = reshape(RESULTS(i).P.psdx,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1,[]);
+        if options.cluster_pixels;
+            P.active_pixels(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) = P.active_pixels(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4)) + ...
+                reshape(RESULTS(i).P.active_pixels,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1);
+            P.psdx(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),:) = reshape(RESULTS(i).P.psdx,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1,[]);
+        end
     else
         b_temp = zeros(sizY(1),sizY(2),sizY(3));
         b_temp(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) = reshape(full(RESULTS(i).b),patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1,patches{i}(6)-patches{i}(5)+1);
         MASK(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) = MASK(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) + 1;
         P.sn(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) = reshape(RESULTS(i).P.sn,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1,patches{i}(6)-patches{i}(5)+1);
-        if isfield(RESULTS(i).P,'sn_ds'); P.sn_ds(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) = reshape(RESULTS(i).P.sn_ds,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1,patches{i}(6)-patches{i}(5)+1);  end      
-        P.active_pixels(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) = P.active_pixels(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) + ...
-            reshape(RESULTS(i).P.active_pixels,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1,patches{i}(6)-patches{i}(5)+1);
+        if isfield(RESULTS(i).P,'sn_ds'); P.sn_ds(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) = reshape(RESULTS(i).P.sn_ds,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1,patches{i}(6)-patches{i}(5)+1);  end             
         IND(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) = IND(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) + 1;
-        P.psdx(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6), :) = reshape(RESULTS(i).P.psdx,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1, patches{i}(6)-patches{i}(5)+1,[]);
+        if options.cluster_pixels
+            P.active_pixels(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) = P.active_pixels(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6)) + ...
+                reshape(RESULTS(i).P.active_pixels,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1,patches{i}(6)-patches{i}(5)+1);
+            P.psdx(patches{i}(1):patches{i}(2),patches{i}(3):patches{i}(4),patches{i}(5):patches{i}(6), :) = reshape(RESULTS(i).P.psdx,patches{i}(2)-patches{i}(1)+1,patches{i}(4)-patches{i}(3)+1, patches{i}(6)-patches{i}(5)+1,[]);
+        end
     end
     P.b = [P.b;RESULTS(i).P.b];
     P.c1 = [P.c1;RESULTS(i).P.c1];
@@ -200,22 +227,23 @@ C(ff,:) = [];
 S(ff,:) = [];
 fprintf(' done. \n');
 
-%RESULTS = remove_boundaries(RESULTS,patches,0.05);
-%% estimate active pixels
-fprintf('Classifying pixels...')
-if length(sizY) == 3
-    X = P.psdx(:,:,1:min(size(P.psdx,3),500));
-else
-    X = P.psdx(:,:,:,1:min(size(P.psdx,4),500));
+if options.cluster_pixels
+    % estimate active pixels
+    fprintf('Classifying pixels...')
+    if length(sizY) == 3
+        X = P.psdx(:,:,1:min(size(P.psdx,3),500));
+    else
+        X = P.psdx(:,:,:,1:min(size(P.psdx,4),500));
+    end
+    X = reshape(X,[],size(X,ndims(X)));
+    X = bsxfun(@minus,X,mean(X,2));     % center
+    X = spdiags(std(X,[],2)+1e-5,0,size(X,1),size(X,1))\X;
+    [L,Cx] = kmeans_pp(X',2);
+    [~,ind] = min(sum(Cx(max(1,end-49):end,:),1));
+    P.active_pixels = (L==ind);
+    P.centroids = Cx;
+    fprintf(' done. \n');
 end
-X = reshape(X,[],size(X,ndims(X)));
-X = bsxfun(@minus,X,mean(X,2));     % center
-X = spdiags(std(X,[],2)+1e-5,0,size(X,1),size(X,1))\X;
-[L,Cx] = kmeans_pp(X',2);
-[~,ind] = min(sum(Cx(max(1,end-49):end,:),1));
-P.active_pixels = (L==ind);
-P.centroids = Cx;
-fprintf(' done. \n');
 %% merge results
 fprintf('Merging overlaping components...')
 Am = A;
@@ -231,29 +259,58 @@ while Km < Kn
     Km = size(Am,2);
 end
 fprintf(' done. \n');
-%% classify components
-%ff = classify_components(Am,Pm,options);
-ff = true(size(Am,2),1);
-A = Am(:,ff);
-C = Cm(ff,:);
 
 %% compute spatial and temporal background using a rank-1 fit
-fin = [mean(F);rand(gnb-1,length(F))];
+fprintf('Computing background components...')
+fin = [mean(F);rand(options.gnb-1,length(F))];
 for iter = 1:150
     fin = diag(sqrt(sum(fin.^2,2)))\fin;
     bin = max(B*(F*fin')/(fin*fin'),0);
     fin = max((bin'*bin)\(bin'*B)*F,0);
 end
+fprintf(' done. \n');
+
+%% classify components
+
+if p == 0
+    % perform deconvolution before classifying components
+    Pm.p = 2;
+    [Cm,fin,Pm,Sm,YrA] = update_temporal_components(data,Am,bin,Cm,fin,Pm,options);
+end
+
+
+if options.classify_comp
+    fprintf('Classifying components...')
+    [rval_space,rval_time,ind_space,ind_time] = classify_comp(data,Am,Cm,bin,fin,options);
+    ind = ind_space & ind_time;
+    fprintf(' done. \n');
+else
+    ind = true(size(Am,2),1);
+    rval_space = NaN(size(Am,2),1);
+    rval_time = NaN(size(Am,2),1);
+end
+
+A = Am(:,ind);
+C = Cm(ind,:);
+
+Pm.rval_space = rval_space;
+Pm.rval_time = rval_time;
+Pm.A_throw = Am(:,~ind);
+Pm.C_throw = Cm(~ind,:);
+
 %% update spatial components
 fprintf('Updating spatial components...');
+options.nb = options.gnb;
 options.d1 = sizY(1);
 options.d2 = sizY(2);
 if length(sizY) == 4; options.d3 = sizY(3); end
+if ~isfield(Pm,'mis_values'); Pm.mis_values = []; end
+if ~isfield(Pm,'mis_entries'); Pm.mis_entries = []; end
 [A,b,C,Pm] = update_spatial_components(data,C,fin,A,Pm,options);
 fprintf(' done. \n');
 %% update temporal components
 fprintf('Updating temporal components... ')
 Pm.p = p;
-options.temporal_iter = 2;
+options.temporal_iter = 1;
 [C,f,P,S,YrA] = update_temporal_components(data,A,b,C,fin,Pm,options);
 fprintf(' done. \n');
---
+++ utilities/classify_comp_corr.m
@@ -0,0 +1,101 @@
+function [rval_space,rval_time,ind_space,ind_time] = classify_comp_corr(Yr,A,C,b,f,options)
+
+% Classify components by looking at how well the spatial and temporal components
+% correlate with the raw data at point of high activation
+
+% INPUTS
+% Yr:       reshaped data in 2D matrix or memmaped file
+% A,C,b,f:  identified components
+% options:  options structure
+%    space_thresh:  r-value threshold for spatial components (default: 0.4) 
+%    time_thresh:  r-value threshold for temporal components (default: 0.4) 
+%    Athresh:  threshold for determining spatial overlap (default: 0.1)
+%    Np:       number of high activity intervals for each component (default: 10)
+%    peak_int: Interval around each local peak to be considered (default: -5:24)
+
+% OUTPUTS:
+% rval_space:     r-values between spatial components and data patches
+% rval_time:      r-values between temporal components and trace averages
+% ind_space:      components with rval_space > space_thresh
+% ind_time:       components with rval_time > time_thresh
+
+% Written by Eftychios A. Pnevmatikakis, Simons Foundation, 2016
+
+defoptions = CNMFSetParms;
+if nargin < 6 || isempty(options)
+    options = defoptions;
+end
+
+if ~isfield(options,'peak_int') || isempty(options.peak_int); int = defoptions.peak_int; else int = options.peak_int; end
+if ~isfield(options,'Npeaks') || isempty(options.peak_int); Np = defoptions.Npeaks; else Np = options.Npeaks; end
+if ~isfield(options,'A_thresh') || isempty(options.A_thresh); Athresh = defoptions.A_thresh; else Athresh = options.A_thresh; end
+if ~isfield(options,'space_thresh') || isempty(options.space_thresh); options.space_thresh = defoptions.space_thresh; end
+if ~isfield(options,'time_thresh') || isempty(options.time_thresh); options.time_thresh = defoptions.time_thresh; end
+
+memmaped = isobject(Yr);
+
+[K_m,T] = size(C);
+pk = zeros(K_m,Np);
+
+parfor i = 1:K_m
+    [~,pk(i,:)] = findpeaks(C(i,:),'SortStr','descend','Npeaks',Np);
+end
+
+%% expand intervals
+lint = length(int);
+
+locs = repmat(pk,1,lint) + kron(int,ones(size(pk)));
+locs = sort(locs,2,'ascend');
+locs(locs<1)=1;
+locs(locs>T)=T;
+LOCS = mat2cell(locs,ones(K_m,1),Np*lint);
+for i = 1:K_m
+    LOCS{i} = unique(LOCS{i});
+end
+
+%% compute r-values
+nA = full(sqrt(sum(A.^2)))';
+tAA = (A'*A)./(nA*nA') > Athresh;
+tAA(1:K_m+1:K_m^2) = 0;
+
+rval_space = zeros(K_m,1);
+rval_time  = zeros(K_m,1);
+data = Yr.Yr;
+for i = 1:K_m
+    ovlp_cmp = find(tAA(:,i));
+    indeces = LOCS{i};
+    for j = 1:length(ovlp_cmp)
+        indeces = setdiff(indeces,LOCS{ovlp_cmp(j)});
+    end
+    amask = A(:,i)>0;
+    if ~isempty(indeces)
+        if memmaped      
+    %         time_indeces = sort(indeces,'ascend');
+    %         ff_time = [0,find(diff(time_indeces)>1),length(time_indeces)];
+    %         time_intervals = mat2cell(time_indeces,1,diff(ff_time));
+    %         space_indeces = sort(find(amask),'ascend')';
+    %         ff_space = [0,find(diff(space_indeces)>1),length(space_indeces)];
+    %         space_intervals = mat2cell(space_indeces,1,diff(ff_space));
+    %         data = zeros(ff_space(end),ff_time(end));
+    %         for rows = 1:length(space_intervals)
+    %             for columns = 1:length(time_intervals)
+    %                 data(ff_space(rows)+1:ff_space(rows+1),ff_time(columns)+1:ff_time(columns+1)) = Yr.Yr(space_intervals{rows},time_intervals{columns});
+    %             end
+    %         end                
+            %mY = (A(:,i)>0).*double(mean(Yr.Yr(:,indeces),2) - b*mean(f(:,indeces),2));
+            mY_space = double(mean(data(amask,indeces),2) - b(amask,:)*mean(f(:,indeces),2));
+            mY_time = double(mean(data(amask,indeces)) - mean(b(amask,:))*f(:,indeces));
+        else
+            mY_space = double(mean(Yr(amask,indeces),2) - b(amask,:)*mean(f(:,indeces),2));
+            mY_time = double(mean(Yr(amask,indeces)) - mean(b(amask,:))*f(:,indeces));
+        end
+        rval_space(i) = corr(full(A(amask,i)),mY_space);
+        rval_time(i) = corr(C(i,indeces)',mY_time');
+    else
+        rval_space(i) = NaN;
+        rval_time(i) = NaN;
+    end
+end
+
+ind_space = rval_space > options.space_thresh;
+ind_time = rval_time > options.time_thresh;
