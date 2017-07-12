8aca6bc094652e4e8632be88223d4421a25f93b1
andrea.giovannucci@gmail.com <andrea.giovannucci@gmail.com>
2017-07-12 17:00:15 -0400

    demo was wrong, fixed

--- use_cases/CajalCourse2017/demo_CajalCourse_2017_neurofinder02.00.m
+++ use_cases/CajalCourse2017/demo_CajalCourse_2017_neurofinder02.00.m
@@ -1,19 +1,10 @@
 clear;
+addpath(genpath('../ca_source_extraction'));  % add packages to matlab path
+addpath(genpath('../NoRMCorre'));
 gcp;    % start a local cluster
 
-filename = 'demoSue2x.tif';
-if ~exist(filename,'file');
-    url = 'https://www.dropbox.com/s/36xdfd28eone0hj/demoSue2x.tif?dl=1';
-    fprintf('downloading the file...');
-    outfilename = websave(filename,url);
-    fprintf('done. \n');
-end
-
-addpath(genpath('ca_source_extraction'));  % add packages to matlab path
-addpath(genpath('NoRMCorre'));
-
+filename = 'use_cases/CajalCourse2017/images_all.tif';
 %% read file and determine dynamic range
-
 Y = read_file(filename);
 [d1,d2,T] = size(Y);    % dimensions of file
 Y = Y - min(Y(:));      % remove negative offset
@@ -22,115 +13,50 @@ minY = quantile(Y(1:1e7),0.0005);
 maxY = quantile(Y(1:1e7),1-0.0005);
 
 %%  play raw movie 
-implay((Y-minY)/(maxY-minY),100)
-
-% figure;
-%     for t = 1:T
-%         imagesc(Y(:,:,t),[minY,maxY]); axis square; axis off; colormap('gray');
-%         title(['Raw data, frame ',num2str(t),' out of ',num2str(T)]);
-%         pause(0.001);
-%     end
+play_movie({Y},{'Raw data'},minY,maxY)
 %% perform motion correction (start with rigid)
+tic
 options_rg = NoRMCorreSetParms('d1',size(Y,1),'d2',size(Y,2),'bin_width',100,'max_shift',15,'us_fac',50);
 [M_rg,shifts_rg,template_rg] = normcorre_batch(Y,options_rg); 
-
-%% view data 
+time_mc_rig = toc
+%% view data (press key to interrupt playing)
 tsub = 5;   % downsampling factor
 Y_sub = downsample_data(Y,'time',tsub);
 M_rgs = downsample_data(M_rg,'time',tsub);
-% you might want to avoid to use the implay command since there is no text with the movies...
-implay(cat(2,Y_sub,M_rgs)/maxY)
-% figure;
-%     for t = 1:size(Y_sub,3)
-%         subplot(121); imagesc(Y_sub(:,:,t),[minY,maxY]); axis square; axis off; colormap('gray');
-%                       title(['Raw data (downsampled), frame ',num2str(t)]);
-%         subplot(122); imagesc(M_rgs(:,:,t),[minY,maxY]); axis square; axis off; colormap('gray');
-%                       title('Rigidly corrected data (downsampled)');
-%         pause(0.001);
-%     end
+
+play_movie({Y_sub,M_rgs},{'Raw data (downsampled)','Rigidly corrected data (downsampled)'},minY,maxY)
+%% clear memory
+
 
 %% perform non-rigid motion correction    
-% parameters motion correction
-% 'd1','d2': size FOV movie
-% 'grid_size','overlap_pre': parameters regulating size of patch (size patch ~ (grid_size + 2*overlap_pre))
-% 'mot_uf': upsampling factor of the grid for shift application
-% 'bin_width': what?
-% 'max_shift': maximum allowed rigid shift
-% 'max_dev': maximum deviation allowed for each patch from the rigid shift value
-% 'us_fac': what?                
+%this does not improve the results
                 
-options_nr = NoRMCorreSetParms('d1',size(Y,1),'d2',size(Y,2),...
-                    'grid_size',[48,48],'mot_uf',4,'overlap_pre',[16,16],...
-                    'bin_width',100,'max_shift',15,'max_dev',8,'us_fac',50);
-
-[M_nr,shifts_nr,template_nr] = normcorre_batch(Y,options_nr,template_rg); 
-                
-%% view (downsampled) data
-
-M_nrs = downsample_data(M_nr,'time',tsub);
-implay(cat(2,Y_sub,M_rgs,M_nrs)/maxY)
-
-% figure;
-%     for t = 1:size(Y_sub,3)
-%         subplot(131); imagesc(Y_sub(:,:,t),[minY,maxY]); axis square; axis off; colormap('gray');
-%                       title('Raw data');
-%         subplot(132); imagesc(M_rgs(:,:,t),[minY,maxY]); axis square; colormap('gray');
-%                       title('Rigidly corrected');
-%                       set(gca,'Xtick',[],'Ytick',[]);
-%                       xlabel(['Frame ',num2str(t)]);
-%         subplot(133); imagesc(M_nrs(:,:,t),[minY,maxY]); axis square; axis off; colormap('gray');
-%                       title('Piecewise-rigidly corrected');
-%         pause(0.001);
-%     end
-    
 %% compute some metrics
-
 [cY,mY,vY] = motion_metrics(Y,options_rg.max_shift);
 [cM_rg,mM_rg,vM_rg] = motion_metrics(M_rg,options_rg.max_shift);
-[cM_nr,mM_nr,vM_nr] = motion_metrics(M_nr,options_rg.max_shift);
-
-
 %% plot metrics
 figure;
-    ax(1) = subplot(2,3,1); imagesc(mY,[minY,maxY]);  axis equal; axis tight; axis off; title('mean raw data','fontsize',14,'fontweight','bold')
-    ax(2) = subplot(2,3,2); imagesc(mM_rg,[minY,maxY]); axis equal; axis tight; axis off; title('mean rigid corrected','fontsize',14,'fontweight','bold')
-    ax(3) = subplot(2,3,3); imagesc(mM_nr,[minY,maxY]); axis equal; axis tight; axis off; title('mean non-rigid corrected','fontsize',14,'fontweight','bold')
-    subplot(2,3,4); plot(1:T,cY,1:T,cM_rg,1:T,cM_nr); legend('raw data','rigid','non-rigid'); title('correlation coefficients','fontsize',14,'fontweight','bold')
-    subplot(2,3,5); scatter(cY,cM_rg); hold on; plot([0.9*min(cY),1.05*max(cM_rg)],[0.9*min(cY),1.05*max(cM_rg)],'--r'); axis square;
+    ax(1) = subplot(2,2,1); imagesc(mY,[minY,maxY]);  axis equal; axis tight; axis off; title('mean raw data','fontsize',14,'fontweight','bold')
+    ax(2) = subplot(2,2,2); imagesc(mM_rg,[minY,maxY]); axis equal; axis tight; axis off; title('mean rigid corrected','fontsize',14,'fontweight','bold')
+    subplot(2,2,3); plot(1:T,cY,1:T,cM_rg); legend('raw data','rigid'); title('correlation coefficients','fontsize',14,'fontweight','bold')
+    subplot(2,2,4); scatter(cY,cM_rg); hold on; plot([0.9*min(cY),1.05*max(cM_rg)],[0.9*min(cY),1.05*max(cM_rg)],'--r'); axis square;
         xlabel('raw data','fontsize',14,'fontweight','bold'); ylabel('rigid corrected','fontsize',14,'fontweight','bold');
-    subplot(2,3,6); scatter(cM_rg,cM_nr); hold on; plot([0.95*min(cM_rg),1.05*max(cM_nr)],[0.95*min(cM_rg),1.05*max(cM_nr)],'--r'); axis square;
-        xlabel('rigid corrected','fontsize',14,'fontweight','bold'); ylabel('non-rigid corrected','fontsize',14,'fontweight','bold');
     linkaxes(ax,'xy')
-%% plot shifts        
-
-shifts_r = squeeze(cat(3,shifts_rg(:).shifts));
-shifts_n = cat(ndims(shifts_nr(1).shifts)+1,shifts_nr(:).shifts);
-shifts_n = reshape(shifts_n,[],ndims(Y)-1,T);
-shifts_x = squeeze(shifts_n(:,2,:))';
-shifts_y = squeeze(shifts_n(:,1,:))';
-
-patch_id = 1:size(shifts_x,2);
-str = strtrim(cellstr(int2str(patch_id.')));
-str = cellfun(@(x) ['patch # ',x],str,'un',0);
-
-figure;
-    ax1 = subplot(311); plot(1:T,cY,1:T,cM_rg,1:T,cM_nr); legend('raw data','rigid','non-rigid'); title('correlation coefficients','fontsize',14,'fontweight','bold')
-            set(gca,'Xtick',[])
-    ax2 = subplot(312); plot(shifts_x); hold on; plot(shifts_r(:,2),'--k','linewidth',2); title('displacements along x','fontsize',14,'fontweight','bold')
-            set(gca,'Xtick',[])
-    ax3 = subplot(313); plot(shifts_y); hold on; plot(shifts_r(:,1),'--k','linewidth',2); title('displacements along y','fontsize',14,'fontweight','bold')
-            xlabel('timestep','fontsize',14,'fontweight','bold')
-    linkaxes([ax1,ax2,ax3],'x')
-
+%%
+clear Y Y_sub M_rgs
+%% correlation image
+Cn = correlation_image_max(M_rg);
+imagesc(Cn)
+axis image
 %% now perform source extraction by splitting the FOV in patches
 
-sizY = size(M_nr);
+sizY = size(M_rg);
 patch_size = [30,30];                   % size of each patch along each dimension (optional, default: [32,32])
 overlap = [8,8];                        % amount of overlap in each dimension (optional, default: [4,4])
 
 patches = construct_patches(sizY(1:end-1),patch_size,overlap);
-K = 4;                                            % number of components to be found
-tau = 4;                                          % std of gaussian kernel (size of neuron) 
+K = 5;                                            % number of components to be found
+tau = 5;                                          % std of gaussian kernel (size of neuron) 
 p = 0;                                            % order of autoregressive system (p = 0 no dynamics, p=1 just decay, p = 2, both rise and decay)
 
 options = CNMFSetParms(...
@@ -148,21 +74,22 @@ options = CNMFSetParms(...
 
 %% run CNMF algorithm on patches and combine
 tic;
-[A,b,C,f,S,P,RESULTS,YrA] = run_CNMF_patches(M_nr,K,patches,tau,p,options);
-[ROIvars.rval_space,ROIvars.rval_time,ROIvars.max_pr,ROIvars.sizeA,keep] = classify_components(M_nr,A,C,b,f,YrA,options);
+[A,b,C,f,S,P,RESULTS,YrA] = run_CNMF_patches(M_rg,K,patches,tau,p,options);
+[ROIvars.rval_space,ROIvars.rval_time,ROIvars.max_pr,ROIvars.sizeA,keep] = classify_components(M_rg,A,C,b,f,YrA,options);
 toc
+%% restart pool here
 
 %% a simple GUI
-Cn = correlation_image_max(M_nr);
 Coor = plot_contours(A,Cn,options,1); close;
-run_GUI = 0;
-if run_GUI
-    GUIout = ROI_GUI(A,options,Cn,Coor,keep,ROIvars);   
-    options = GUIout{2};
-    keep = GUIout{3};    
-end
+% run_GUI = 0;
+% if run_GUI
+%     GUIout = ROI_GUI(A,options,Cn,Coor,keep,ROIvars);   
+%     options = GUIout{2};
+%     keep = GUIout{3};    
+% end
 
 %% view contour plots of selected and rejected components (optional)
+keep = (ROIvars.rval_space>.7 & ROIvars.rval_time>0);
 throw = ~keep;
 figure;
     ax1 = subplot(121); plot_contours(A(:,keep),Cn,options,0,[],Coor,1,find(keep)); title('Selected components','fontweight','bold','fontsize',14);
@@ -174,9 +101,9 @@ plot_components_GUI(M_nr,A(:,keep),C(keep,:),b,f,Cn,options);
 %% refine temporal components
 A_keep = A(:,keep);
 C_keep = C(keep,:);
-[C2,f2,P2,S2,YrA2] = update_temporal_components(reshape(M_nr,[],T),A_keep,b,C_keep,f,P,options);
+[C2,f2,P2,S2,YrA2] = update_temporal_components(reshape(M_rg,[],T),A_keep,b,C_keep,f,P,options);
 
-%% detrend fluorescence and extract DF/F values
+git %% detrend fluorescence and extract DF/F values
 df_percentile = 30;
 window = 1000; 
 
@@ -198,13 +125,11 @@ for i = 1:nNeurons
 end
 
 %% plot a random component
-
 i = randi(nNeurons);
-
 figure;plot(1:T,F_dff(i,:),'-k'); hold all; plot(1:T,C_dec(i,:),'r','linewidth',2);
     spt = find(S(i,:));
     if spt(1) == 1; spt(1) = []; end
     hold on; scatter(spt,repmat(-0.25,1,length(spt)),'m*')
     title(['Component ',num2str(i)]);
-    
+    ls ..Cajal
     legend('Fluorescence DF/F','Deconvolved','Spikes')
