66a141b38cf55fecf72abf30ff528457664163fb
epnev <eap2111@columbia.edu>
2017-07-19 11:46:38 +0100

    improvements

--- use_cases/2017_CSHL_NeuralDataScience/demo_CSHL2017_neurofinder02.00.m
+++ use_cases/2017_CSHL_NeuralDataScience/demo_CSHL2017_neurofinder02.00.m
@@ -3,36 +3,50 @@ addpath(genpath('../../../ca_source_extraction'));  % add packages to matlab pat
 addpath(genpath('../../../NoRMCorre'));
 gcp;    % start a local cluster
 
-foldername = '/mnt/xfs1/home/agiovann/Downloads/neurofinder.02.00/images';
+foldername = '/Users/epnevmatikakis/Documents/Ca_datasets/Neurofinder/neurofinder.02.00/images';
     % change foldername to where the data is saved 
-files = subdir(fullfile(foldername,'*.tif*'));
+files = subdir(fullfile(foldername,'image*.tif*'));
 numFiles = length(files)
 
-%% read neurofinder
+%% read neurofinder frames and combine them in a single tiff file
 tic
-delete(fullfile(foldername,'neurofinder0200.tif'))
-Ycon = read_neurofinder(foldername,fullfile(foldername,'neurofinder0200.tif'));
-% Ycon = read_neurofinder(foldername,[]);
+if exist(fullfile(foldername,'neurofinder0200.tif'),'file')
+    Ycon = read_file(fullfile(foldername,'neurofinder0200.tif'),1,1000);    
+        % just read a few frames if it exists
+else
+    Ycon = read_neurofinder(foldername,fullfile(foldername,'neurofinder0200.tif'));
+end
 toc
-%%
+%% get dynamic range for showing movies
 minY = quantile(Ycon(1:1e7),0.0005);
 maxY = quantile(Ycon(1:1e7),1-0.0005);
 %%
 play_movie({Ycon},{'Y'},minY,maxY)
-% clear Y_con
-%% rigid motion correct
-options_rg = NoRMCorreSetParms('d1',size(Ycon,1),'d2',size(Ycon,2),'bin_width',100,'max_shift',15);
-delete(options_rg.mem_filename)
-[M_rg,shifts_rg,template_rg] = normcorre_batch(single(Ycon),options_rg); 
+clear Ycon
+%% perform rigid motion correction and save output as a tiff file
+options_rg = NoRMCorreSetParms('d1',512,'d2',512,...
+        'bin_width',200,'max_shift',15,'output_type','tif',...
+        'tiff_filename',fullfile(foldername,'neurofinder0200_rig.tif'));
+
+if ~exist(fullfile(foldername,'neurofinder0200_rig.tif'),'file') % check if file exists
+    [M_rg,shifts_rg,template_rg] = normcorre_batch(fullfile(foldername,'neurofinder0200.tif'),options_rg); 
+end
+
+
 %% construct a memory mapped file
+
 tic;
-data = memmap_file(fullfile(foldername,'neurofinder0200.tif'));
-toc
+if ~exist(fullfile(foldername,'neurofinder0200_rig.mat'),'file')
+    data = memmap_file(options_rg.tiff_filename);
+else
+    data = matfile(fullfile(foldername,'neurofinder0200_rig.mat'));
+end
 
 %% now perform source extraction by splitting the FOV in patches
+
 sizY = size(data,'Y');
 patch_size = [32,32];                   % size of each patch along each dimension (optional, default: [32,32])
-overlap = [10,10];                        % amount of overlap in each dimension (optional, default: [4,4])
+overlap = [5,5];                      % amount of overlap in each dimension (optional, default: [4,4])
 
 patches = construct_patches(sizY(1:end-1),patch_size,overlap);
 K = 5;                                            % number of components to be found
@@ -41,30 +55,42 @@ p = 0;                                            % order of autoregressive syst
 
 options = CNMFSetParms(...
     'd1',sizY(1),'d2',sizY(2),...
-    'deconv_method','constrained_foopsi',...    % activity deconvolution method
     'temporal_iter',2,...                       % number of block-coordinate descent steps 
-    'cluster_pixels',false,...  
     'ssub',2,...                                % downsample factor in space
     'tsub',4,...                                % downsample factor in time
     'merge_thr',0.8,...                         % merging threshold
     'gSig',tau,... 
-    'gnb',2,...
+    'gnb',2,...                                 % number of background components
     'spatial_method','regularized'...
     );
 
+
 %% run CNMF algorithm on patches, combine results and classify components
+
 tic;
 [A,b,C,f,S,P,RESULTS,YrA] = run_CNMF_patches(data,K,patches,tau,p,options);
 [ROIvars.rval_space,ROIvars.rval_time,ROIvars.max_pr,ROIvars.sizeA,keep] = classify_components(data,A,C,b,f,YrA,options);
 toc
-%%
+
+%% compute correlation image 
 Cn = correlation_image_max(data);
+
 %% a simple GUI for further classification
 Coor = plot_contours(A,Cn,options,1); close;
-traces = C+YrA;
-[fitness,erfc,sd_r,md] = compute_event_exceptionality(traces,0);
+%traces = C+YrA;
+%tic; [fitness,erfc,sd_r,md] = compute_event_exceptionality(traces,0); toc
+
+%% 
+run_GUI = false;
+if run_GUI
+    GUIout = ROI_GUI(A,options,Cn,Coor,keep,ROIvars);   
+    options = GUIout{2};
+    keep = GUIout{3};    
+end
+
+
 %% view contour plots of selected and rejected components (optional)
-keep = (ROIvars.rval_space>.8 | fitness<-50);
+%keep = (ROIvars.rval_space>.8 | fitness<-50);
 throw = ~keep;
 figure;
     ax1 = subplot(121); plot_contours(A(:,keep),Cn,options,0,[],Coor,1,find(keep)); title('Selected components','fontweight','bold','fontsize',14);
