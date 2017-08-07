3bafe12ecb24877c4150802f6dfb041edea51401
Jérémie Kalfon <jkobject@gmail.com>
2017-08-07 12:07:57 -0400

    formatting the OO version of the pipeline to make it more Object oriented. adding new functions in it and changing the pipeline  and debugging it

--- demo_script_class.m
+++ demo_script_class.m
@@ -4,102 +4,124 @@ clear;
 %% load file
 
 addpath(genpath('utilities'));
-             
-nam = 'demoMovie.tif';          % insert path to tiff stack here
-sframe=1;						% user input: first frame to read (optional, default 1)
-num2read=2000;					% user input: how many frames to read   (optional, default until the end)
+addpath(genpath('deconvolution'));
+Source = Sources2D;
+Source.file = 'demoMovie.tif';  % insert path to tiff stack here  
 
-Y = bigread2(nam,sframe,num2read);
-if ~isa(Y,'double');    Y = double(Y);  end         % convert to double
-Y = Y - min(Y(:));                                  % make data non-negative
-
-[d1,d2,T] = size(Y);                                % dimensions of dataset
-d = d1*d2;                                          % total number of pixels
-
-%% Set parameters
-
-K = 30;                                           % number of components to be found
-tau = 4;                                          % std of gaussian kernel (size of neuron) 
-p = 2;                                            % order of autoregressive system (p = 0 no dynamics, p=1 just decay, p = 2, both rise and decay)
-merge_thr = 0.8;                                  % merging threshold
-
-obj = Sources2D;
-updateParams(obj,...            
-    'd1',d1,'d2',d2,...                         % dimensions of datasets
+Source.updateParams(...                                    % dimensions of datasets
     'search_method','ellipse','dist',3,...      % search locations when updating spatial components
     'deconv_method','constrained_foopsi',...    % activity deconvolution method
     'temporal_iter',2,...                       % number of block-coordinate descent steps 
     'fudge_factor',0.98,...                      % bias correction for AR coefficients
-    'merge_thr',merge_thr...                    % merging threshold
-    );
+    'merge_thr',0.8...                    % merging threshold
+    ); 
 
+sframe=1;						% user input: first frame to read (optional, default 1)
+num2read=2000;					% user input: how many frames to read   (optional, default until the end)
+Source.read(sframe,num2read)
+
+%%%% MOTION CORRECTION
+%todo integrate motion correction
+                                     
 %% Data pre-processing
-
-Y = preprocess(obj,Y,p);
+p = 2;    % order of autoregressive system (p = 0 no dynamics, p=1 just decay, p = 2, both rise and deca 
+Source.preprocess(p);
 
 %% fast initialization of spatial components using greedyROI and HALS
-
-center = initComponents(obj, Y, K, tau);
+K = 30; tau = 4;  % number of components to be found     % std of gaussian kernel (size of neuron) 
+Source.initComponents(K, tau);
 
 % display centers of found components
-Cn =  reshape(obj.P.sn,d1,d2); %correlation_image(Y); %max(Y,[],3); %std(Y,[],3); % image statistic (only for display purposes)
-figure;imagesc(Cn);
+figure;imagesc(Source.Cn);
     axis equal; axis tight; hold all;
-    scatter(center(:,2),center(:,1),'mo');
+    scatter(Source.cm(:,2),Source.cm(:,1),'mo');
     title('Center of ROIs found from initialization algorithm');
     drawnow;
 
+%%%%%%%%%%%%%%%introduction to GUI mode ;
+%% run GUI will let you, refine the components manually, change the parameters and see the results,
+% analyse the trace of each components, use a classification algorithm to
+% find the components instead, add and remove components, save the ROIS
+% and finish the pipeline with button clicks. 
+
+%it is still an optional method.
+run_GUI = false;
+if run_GUI
+    %todo to implement 
+    ROIvars.C = obj.C;
+    Coor = plot_contours(obj.A,obj.Cn,obj.options,1); close;
+    % here is what the GUI needs to receive in parameters
+    GUIout = ROI_GUI(obj.Y ,obj.A ,obj.P ,obj.options ,obj.Cn ,Coor ,...
+        obj.keep ,ROIvars ,obj.b ,obj.f ,obj.S,obj.Yra);   
+    options = GUIout{2};
+    keep = GUIout{3};    
+end
+
+%%OR
 %% manually refine components (optional)
-refine_components = true;  % flag for manual refinement
+refine_components = false;  % flag for manual refinement
 if refine_components
-    [center] = refineComponents(Y,obj,center,Cn,tau);
+    Source.refineComponents();
 end
     
+Source.Y = []; %% you might want to free up memory if it is a large file and you do this update spatial.
 %% update spatial components
-Yr = reshape(Y,d,T);
-clear Y;
-updateSpatial(obj, Yr);
-
+Source.updateSpatial();
 %% update temporal components
-updateTemporal(obj, Yr);
+Source.updateTemporal();
 
+%%%%%%%%%%%%%%%% classify +traces
+Source.classifyComponents();
+Source.compute_event_exceptionality();
 %% merge found components
-Apr = obj.A;    % store non-merged components
-Cpr = obj.C;
-[K_m, merged_ROIs] = merge(obj, Yr);
-display_merging = 1; % flag for displaying merging example
+Apr = Source.A;    % store non-merged components
+Cpr = Source.C;
+Source.merge();
+
+display_merging = true; % flag for displaying merging example
+try
+    i = 1; randi(length(Source.ROI));
+catch
+    disp('no merged ROIS');
+    display_merging = false;
+end
 if display_merging
-    i = 1; randi(length(merged_ROIs));
-    ln = length(merged_ROIs{i});
+    ln = length(Source.ROI{i});
     figure;
         set(gcf,'Position',[300,300,(ln+2)*300,300]);
         for j = 1:ln
-            subplot(1,ln+2,j); imagesc(reshape(Apr(:,merged_ROIs{i}(j)),d1,d2)); 
+            subplot(1,ln+2,j); imagesc(reshape(Apr(:,Source.ROI{i}(j)),Source.options.d1,Source.options.d2)); 
                 title(sprintf('Component %i',j),'fontsize',16,'fontweight','bold'); axis equal; axis tight;
         end
-        subplot(1,ln+2,ln+1); imagesc(reshape(obj.A(:,K_m-length(merged_ROIs)+i),d1,d2));
+        subplot(1,ln+2,ln+1); imagesc(reshape(Source.A(:,Source.K-length(Source.ROI)+i),Source.options.d1,Source.options.d2));
                 title('Merged Component','fontsize',16,'fontweight','bold');axis equal; axis tight; 
         subplot(1,ln+2,ln+2);
-            plot(1:T,(diag(max(Cpr(merged_ROIs{i},:),[],2))\Cpr(merged_ROIs{i},:))'); 
-            hold all; plot(1:T,obj.C(K_m-length(merged_ROIs)+i,:)/max(obj.C(K_m-length(merged_ROIs)+i,:)),'--k')
+            plot(1:T,(diag(max(Cpr(Source.ROI{i},:),[],2))\Cpr(Source.ROI{i},:))'); 
+            hold all; plot(1:Source.T,Source.C(Source.K-length(Source.ROI)+i,:)/max(Source.C(Source.K-length(Source.ROI)+i,:)),'--k')
             title('Temporal Components','fontsize',16,'fontweight','bold')
         drawnow;
 end
 
 %% repeat
-updateSpatial(obj, Yr);
-updateTemporal(obj, Yr);
+Source.updateSpatial();
+Source.updateTemporal();
 %% do some plotting
-[srt] = orderROIs(obj);     % order components
-K_m = size(obj.C,1);
-[C_df, ~] = extractDF_F(obj, Yr); % extract DF/F values.
+Source.orderROIs();     % order components
+Source.extractDF_F(); % extract DF/F values.
+
+%%%%%%%%%%%%%% deconvolve
+Cdec = Source.deconvTemporal();
 
 contour_threshold = 0.95;   % amount of energy used for each component to construct contour plot
 figure;
-[json_file] = viewContours(obj, Cn, contour_threshold, 1);
-%savejson('jmesh',json_file,'filename');        % optional save json file with component coordinates (requires matlab json library)
+Source.viewContours(contour_threshold, 1);
 
-plotComponentsGUI(obj, Yr, Cn);     % display all components
+%%%%%%% save
+save = false;
+if save
+    savejson('jmesh',Source.json_file,'filename');        % optional save json file with component coordinates (requires matlab json library)
+end
+Source.plotComponentsGUI();     % display all components
 pause;
 %% make movie
-makePatchVideo(obj, Yr) 
+Source.makePatchVideo() 
