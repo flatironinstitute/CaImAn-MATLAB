flatironinstitute/CaImAn-MATLAB

commit 9e524e29f17d35db3c79a87de32209c7dccda186

Update determine_search_location.m
explore full FOV when using `dilate` method for computing search location #100 
thanks to @wenxuanliang

utilities/determine_search_location.m
@@ -95,7 +95,7 @@ else
                    ind_t = sqrt((cor*V(:,1)).^2/d11 + (cor*V(:,2)).^2/d22 + (cor*V(:,3)).^2/d33)<=dist;       % search indeces for each component
                end               
            case 'dilate'
-               A_temp = imdilate(reshape(full(A(idx,i)),siz_p),expandCore);
+               A_temp = imdilate(reshape(full(A(:,i)), [d1,d2,d3]), expandCore);
                ind_t = A_temp(:)>0;
        end
        ind_temp = sparse(idx,1,ind_t,d,1); 

commit 49b7884e93348d50df7173e1619d7499468bb1f6

fix bug introduced with 9e524e29f17d35db3c79a87de32209c7dccda186

register_ROIs.m
@@ -58,7 +58,7 @@ siz = [options.d1,options.d2,options.d3];
 options_mc.correct_bidir = false;
 if align_flag
     options_mc.upd_template = false;
-    options_mc.boundary = 'zero';
+    options_mc.boundary = 'copy';
     [~,global_shift] = normcorre(template2,options_mc,template1);
     %global_shift(1).diff = 0*global_shift(1).diff;
     

utilities/determine_search_location.m
@@ -93,13 +93,13 @@ else
                else
                    d33 = min((max_size/2)^2,max((min_size/2)^2,D(3,3)));
                    ind_t = sqrt((cor*V(:,1)).^2/d11 + (cor*V(:,2)).^2/d22 + (cor*V(:,3)).^2/d33)<=dist;       % search indeces for each component
-               end               
+               end
+               ind_temp = sparse(idx,1,ind_t,d,1); 
+               IND(:,i) = logical(ind_temp); 
            case 'dilate'
                A_temp = imdilate(reshape(full(A(:,i)), [d1,d2,d3]), expandCore);
-               ind_t = A_temp(:)>0;
+               IND(:,i) = A_temp(:)>0;
        end
-       ind_temp = sparse(idx,1,ind_t,d,1); 
-       IND(:,i) = logical(ind_temp);       
    end
 end
 